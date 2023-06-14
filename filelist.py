5021f239b5546e91ac1f3fa34000d966cd7574fc
Thomas Kirckof <105011940+kircktd@users.noreply.github.com>
2023-06-14 13:25:48 -0400

    Update filelist.py

--- filelist.py
+++ filelist.py
@@ -26,15 +26,9 @@ hs_config = r"C:\Reporting\Scripts\hs_config.txt"
 ps_script = r"C:\Reporting\Scripts\user_files.ps1"
 user_files = r"C:\Reporting\Scripts\user_files.txt"
 
-file_names = {user_list, file_list}
-for empty_file in file_names:
-    with open(empty_file, "w") as f:
-        f.write("")
-
 #Print Script Startup Message To Screen
 print("Hammerspace Custom Reporting User File Listing Script")
 
-#Subprocess Definition 
 def run_subprocess(command):
     output = ''
     try:
@@ -53,48 +47,58 @@ def filter_output(output):
             with open(user_list, 'a') as list_out:
                  print(user, file=list_out)
 
-with open(hs_config,'r') as hs:
-        for dir in hs:
-            hscmd = "hs usage user"
-            share = dir.strip().replace(":", "").replace('\n', '')
-            cmd = (hscmd + " " + dir)
-            output = run_subprocess(cmd)
-            filter_output(output.decode())
+while True:
+    now = datetime.datetime.now()
+    if now.minute == 50:
+        file_names = {user_list, file_list}
+        for empty_file in file_names:
+            with open(empty_file, "w") as f:
+                f.write("")
 
-with open(user_list, 'r') as file:
-          lines = file.readlines()
+        with open(hs_config,'r') as hs:
+            for dir in hs:
+                hscmd = "hs usage user"
+                share = dir.strip().replace(":", "").replace('\n', '')
+                cmd = (hscmd + " " + dir)
+                output = run_subprocess(cmd)
+                filter_output(output.decode())
 
-unique_lines = []
+        with open(user_list, 'r') as file:
+            lines = file.readlines()
 
-for line in lines:
-     if line not in unique_lines:
-          unique_lines.append(line)
+        unique_lines = []
 
-with open(user_list, 'w') as file:
-     file.writelines(unique_lines)
+        for line in lines:
+            if line not in unique_lines:
+                unique_lines.append(line)
 
-with open(user_list, 'r') as file:
-        lines = file.readlines()  # Read all the lines from the file
+        with open(user_list, 'w') as file:
+            file.writelines(unique_lines)
 
-with open(user_list, 'w', encoding="utf-8") as file:
-     for line in lines:
-          index = line.find('@')
-          if index != -1:
-               line = line[:index] +'\n'
-               line = line.replace("'", "")
-          file.write(line)
+        with open(user_list, 'r') as file:
+            lines = file.readlines()  # Read all the lines from the file
 
-subprocess.run(["powershell.exe", "-File", ps_script], shell=True)
+        with open(user_list, 'w', encoding="utf-8") as file:
+            for line in lines:
+                index = line.find('@')
+                if index != -1:
+                    line = line[:index] +'\n'
+                    line = line.replace("'", "")
+                file.write(line)
 
-with open(user_files, "r") as userfiles:
-    for line in userfiles:
-         line = line.rstrip()
-         data = line.split(',')
-         if len(data) >= 3:
-                file_path = data[0].strip().replace('\\','\\\\').lstrip()
-                file_size = data[1].strip()
-                file_size = file_size.replace(" ", "")
-                user_name = data[2].strip()
-                user_name = user_name.replace(" ", "")
-                with open(file_list, "a") as fileout:
-                    print("fileshare_file_list{File=","\"",file_path,"\"",",","User=","\"",user_name,"\"","}",file_size, file=fileout)
+        subprocess.run(["powershell.exe", "-File", ps_script], shell=True)
+
+        with open(user_files, "r") as userfiles:
+            for line in userfiles:
+                line = line.rstrip()
+                data = line.split(',')
+                if len(data) >= 3:
+                    file_path = data[0].strip().replace('\\','\\\\').lstrip()
+                    file_size = data[1].strip()
+                    file_size = file_size.replace(" ", "")
+                    user_name = data[2].strip()
+                    user_name = user_name.replace(" ", "")
+                    with open(file_list, "a") as fileout:
+                        print("fileshare_file_list{File=","\"",file_path,"\"",",","User=","\"",user_name,"\"","}",file_size, file=fileout)
+
+    time.sleep(60)
